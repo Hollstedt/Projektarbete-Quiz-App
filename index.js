3e527b32972b9e25631c09b5593fe11a9ed1e19d
Daniel Hollstedt <daniel.hollstedt@yh.nackademin.se>
2024-12-06 11:30:07 +0100

    lagt till array med objekt för fråga/svar, deklarerat räknare för poäng och index, delvis lagt till funktion för att visa fråga och stämma av svar

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
+
+// light-dark-mode-toggle
 let lightDarkToggle = document.querySelector('.light-dark-mode');
 
 lightDarkToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+// en array med objekt för fråga och svar
+let questions = [
+    {
+        text: "Den här frågan är true",
+        answer: true
+    },
+    {
+        text: "Den här frågan är false",
+        answer: false
+    },
+    {
+        text: "Den här frågan är true",
+        answer: true
+    },
+]
+
+// räknare för poäng
+let score = 0;
+
+// index för aktuell fråga
+let questionIndex = 0;
+
+function showQuestion() {
+    if (questionIndex < questions.length) {
+        console.log("nu är du i if på showQuestion");
+        document.querySelector('.question-number').textContent = questionIndex + 1;
+        document.querySelector('.question').textContent = questions[questionIndex].text;
+    } else {
+        console.log("nu är du i else på showQuestion");
+        document.querySelector('.score').textContent = `Ditt resultat är ${score} av ${questions.length}.`
+    }
+}
+
+function checkAnswer(userInputAnswer) {
+    if (userInputAnswer === questions[questionIndex].answer) {
+        console.log("Rätt svar! nu är du i if på checkAnswer");
+        score++;
+    } else {
+        console.log("Fel svar! nu är du i else på checkAnswer");
+    }
+    questionIndex++;
+    showQuestion();
+}
+
+// ger argument till kommande funktion beroende på knappval för sant/falskt
+document.querySelector('.true-btn').addEventListener('click', () => checkAnswer(true));
+document.querySelector('.false-btn').addEventListener('click', () => checkAnswer(false));
+
+showQuestion();
