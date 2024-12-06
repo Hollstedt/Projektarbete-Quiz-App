5c17bf35124a7c350208cdb79ccafad3531409a4
Daniel Hollstedt <daniel.hollstedt@yh.nackademin.se>
2024-12-06 15:10:00 +0100

    gjort klart sant/falskt-funktionerna, lagt till DOM-ändringar, fått idé om styling och kommer göra klart för betyg G, sedan skapa ny branch för fortsatt äventyr

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ lightDarkToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
 
+let time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit", hour12: false });
+document.querySelector('.time').textContent = time;
+
 // en array med objekt för fråga och svar
 let questions = [
     {
@@ -16,10 +19,10 @@ let questions = [
         text: "Den här frågan är false",
         answer: false
     },
-    {
-        text: "Den här frågan är true",
-        answer: true
-    },
+    // {
+    //     text: "Den här frågan är true",
+    //     answer: true
+    // },
 ]
 
 // räknare för poäng
@@ -28,6 +31,11 @@ let score = 0;
 // index för aktuell fråga
 let questionIndex = 0;
 
+// dynamisk maxgräns på antal frågor
+document.querySelector('.max-questions').textContent = questions.length;
+
+
+// kollar om det finns frågor att visa och ändrar DOM baserat på cond
 function showQuestion() {
     if (questionIndex < questions.length) {
         console.log("nu är du i if på showQuestion");
@@ -35,10 +43,13 @@ function showQuestion() {
         document.querySelector('.question').textContent = questions[questionIndex].text;
     } else {
         console.log("nu är du i else på showQuestion");
-        document.querySelector('.score').textContent = `Ditt resultat är ${score} av ${questions.length}.`
+        document.querySelector('.quiz-header').textContent = "Nu är det slut på frågor!";
+        document.querySelector('.question').textContent = "";
+        document.querySelector('.score').textContent = `Ditt resultat är ${score} av ${questions.length}.`;
     }
 }
 
+// tar in svaret med argument från eventlistener och om det stämmer överens med svaret i objektet för indexet så score++, annars inget just nu
 function checkAnswer(userInputAnswer) {
     if (userInputAnswer === questions[questionIndex].answer) {
         console.log("Rätt svar! nu är du i if på checkAnswer");
@@ -50,8 +61,9 @@ function checkAnswer(userInputAnswer) {
     showQuestion();
 }
 
-// ger argument till kommande funktion beroende på knappval för sant/falskt
+// anropar checkAnswer-funktionen och skickar argument beroende på knappval för sant/falskt
 document.querySelector('.true-btn').addEventListener('click', () => checkAnswer(true));
 document.querySelector('.false-btn').addEventListener('click', () => checkAnswer(false));
+
 
 showQuestion();
