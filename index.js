1928a3d815dea07574802d315711b01ee52bdac9
Daniel Hollstedt <daniel.hollstedt@gmail.com>
2024-12-09 11:18:43 +0100

    Gjort färdigt conditionals för svar. Ordnat light/dark-mode. Helt klar med kraven för g.

--- index.js
+++ index.js
@@ -19,10 +19,14 @@ let questions = [
         text: "Den här frågan är false",
         answer: false
     },
-    // {
-    //     text: "Den här frågan är true",
-    //     answer: true
-    // },
+    {
+        text: "Den här frågan är true",
+        answer: true
+    },
+    {
+        text: "Den här frågan är false",
+        answer: false
+    },
 ]
 
 // räknare för poäng
@@ -37,6 +41,9 @@ document.querySelector('.max-questions').textContent = questions.length;
 
 // kollar om det finns frågor att visa och ändrar DOM baserat på cond
 function showQuestion() {
+    let phoneScreen = document.querySelector('.phone');
+    let scorePercentage = (score / questions.length) * 100;
+
     if (questionIndex < questions.length) {
         console.log("nu är du i if på showQuestion");
         document.querySelector('.question-number').textContent = questionIndex + 1;
@@ -45,7 +52,22 @@ function showQuestion() {
         console.log("nu är du i else på showQuestion");
         document.querySelector('.quiz-header').textContent = "Nu är det slut på frågor!";
         document.querySelector('.question').textContent = "";
-        document.querySelector('.score').textContent = `Ditt resultat är ${score} av ${questions.length}.`;
+        document.querySelector('.bottom-half').style.display = "none";
+        
+        console.log(scorePercentage);
+
+        // conditional för output av score-procent
+        if (scorePercentage < 50) {
+            let phoneScreen = document.querySelector('.phone');
+            phoneScreen.style.backgroundColor = "#f44336";
+            document.querySelector('.score').innerHTML = `Du fick ${score} av totalt ${questions.length} rätt. <br>Det är inte kanon.`
+        } else if (scorePercentage >= 50 && scorePercentage <= 75) {
+            phoneScreen.style.backgroundColor = "orange";
+            document.querySelector('.score').innerHTML = `Du fick ${score} av totalt ${questions.length} rätt. <br>Det är inte dåligt. Men verkligen inte bra heller.`
+        } else {
+            phoneScreen.style.backgroundColor = "#4caf50";
+            document.querySelector('.score').innerHTML = `Du fick ${score} av totalt ${questions.length} rätt. <br>Det här kan inte anses vara annat än succé!`
+        }
     }
 }
 
