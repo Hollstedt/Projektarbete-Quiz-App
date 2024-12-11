cec0e4100b0c3ced048bc366d8238875719992e6
Daniel Hollstedt <daniel.hollstedt@yh.nackademin.se>
2024-12-11 21:47:43 +0100

    lagt till ljud för skoj, både bakgrund och ljudeffekt baserat på resultat. Sett över koden och försökt städa upp.

--- index.js
+++ index.js
@@ -6,109 +6,132 @@ lightDarkToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
 
+
+// hämtar aktuell tid från date-objekt, initierar variabel för time
 let time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit", hour12: false });
 document.querySelector('.time').textContent = time;
 
-// en array med objekt för fråga och svar
+
+// Musikknappen, initierar variabler för knapp och ljud
+let music = document.querySelector('#menti-music');
+let isPlaying = false;
+
+document.querySelector('.music-button').addEventListener('click', () => {
+    if (isPlaying) {
+        music.pause();
+        document.querySelector('.music-button').textContent = "🙉";
+        isPlaying = false;
+    } else {
+        music.play();
+        document.querySelector('.music-button').textContent = "MAKE IT STOP!";
+        isPlaying = true;
+    }
+});
+
+// en array med objekt för frågor och svar
 let questions = [
     {
-        text: "Den här frågan är true",
+        text: "Ett moln väger cirka 1 miljon ton.",
         answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
-    },
-    {
-        text: "Den här frågan är true",
+        text: "Bär du slips kan det minska din blodtillförsel till hjärnan med upp till 7,5%.",
         answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "Vindturbiner dödar mellan 10,000 och 100,000 fåglar per år.",
+        answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "1 av 2,500 personer känner inte att dom är gravida förrän förlossningen startar.",
+        answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "Kampsportare som ler precis innan matchen startar löper större risk att förlora.",
+        answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "En blixt är fem gånger varmare än solens yta.",
+        answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "Du kan inte vika ett A4-papper fler än 8 gånger.",
+        answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "När första filmen om Star Wars kom ut så var Giljotinen fortfarande Frankrikes främsta verktyg för avrättning.",
+        answer: true
     },
     {
-        text: "Den här frågan är false",
-        answer: false
+        text: "Man åkte till månen innan resväska på hjul hade uppfunnits.",
+        answer: true
+    },
+    {
+        text: "I ett rum med 23 personer är det 50% chans att du delar födelsedag med en av dem.",
+        answer: true
     },
 ]
 
-// räknare för poäng
+// initierar räknare för användarens poäng
 let score = 0;
 
-// index för aktuell fråga
+// initierar index för att hålla koll på vilken fråga som visas
 let questionIndex = 0;
 
-// dynamisk maxgräns på antal frågor
+// dynamisk maxgräns på antal frågor totalt i quizet, baserat på antalet frågor i array
 document.querySelector('.max-questions').textContent = questions.length;
 
+//deklarerar variabel för ljud, för användning i showQuestion baserat på användarens resultat
+let sound;
 
 // kollar om det finns frågor att visa och ändrar DOM baserat på conditional
 function showQuestion() {
-    let phoneScreen = document.querySelector('.phone');
     let scorePercentage = (score / questions.length) * 100;
-
+    
     if (questionIndex < questions.length) {
-        console.log("nu är du i if på showQuestion");
         document.querySelector('.question-number').textContent = questionIndex + 1;
         document.querySelector('.question').textContent = questions[questionIndex].text;
+
+        if (questionIndex === questions.length - 1) {
+            music.pause();
+            document.querySelector('.music-button').textContent = "🙉";
+        }
+
     } else {
-        console.log("nu är du i else på showQuestion");
         document.querySelector('.quiz-header').textContent = "Nu är det slut på frågor!";
         document.querySelector('.question').textContent = "";
         document.querySelector('.bottom-half').style.display = "none";
         document.querySelector('.score').style.display = "block";
-        
-        console.log(scorePercentage);
 
         // conditional för output av score-procent
         if (scorePercentage < 50) {
-            let phoneScreen = document.querySelector('.phone');
-            phoneScreen.style.backgroundColor = "#f44336";
+            document.querySelector('.phone').style.backgroundColor = "#f44336";
             document.querySelector('.score').innerHTML = `Du fick ${score} av ${questions.length} rätt.`;
-            document.querySelector('.score-feedback').textContent = "Det är inte kanon det här."
+            document.querySelector('.score-feedback').textContent = `Det är inte kanon det här.`
+            sound = document.querySelector('#bad-sound');
         } else if (scorePercentage >= 50 && scorePercentage <= 75) {
-            phoneScreen.style.backgroundColor = "orange";
+            document.querySelector('.phone').style.backgroundColor = "orange";
             document.querySelector('.score').innerHTML = `Du fick ${score} av ${questions.length} rätt.`;
-            document.querySelector('.score-feedback').innerHTML = `Det är inte dåligt... <br> men verkligen inte bra heller.`;
+            document.querySelector('.score-feedback').innerHTML = `Det var inte bra och inte dåligt.`;
         } else {
-            phoneScreen.style.backgroundColor = "#4caf50";
+            document.querySelector('.phone').style.backgroundColor = "#4caf50";
             document.querySelector('.score').innerHTML = `Du fick ${score} av ${questions.length} rätt.`;
-            document.querySelector('.score-feedback').textContent = "Det är inte kanon det här."
+            document.querySelector('.score-feedback').innerHTML = `Otroligt bra.`
+            sound = document.querySelector('#great-sound');
+        }
+        
+        if (sound) {
+            sound.play();
         }
     }
 }
 
-// tar in svaret med argument från eventlistener och om det stämmer överens med svaret i objektet för indexet så score++, annars inget just nu
+// tar in svaret med argument från eventlistener och om det stämmer överens med svaret i objektet för indexet så ökar score++ med 1
 function checkAnswer(userInputAnswer) {
-    if (userInputAnswer === questions[questionIndex].answer) {
-        console.log("Rätt svar! nu är du i if på checkAnswer");
-        score++;
-    } else {
-        console.log("Fel svar! nu är du i else på checkAnswer");
-    }
+    if (userInputAnswer === questions[questionIndex].answer) score++; {
     questionIndex++;
     showQuestion();
+    }
 }
 
 // anropar checkAnswer-funktionen och skickar argument beroende på knappval för sant/falskt
